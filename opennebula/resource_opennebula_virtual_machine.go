00e6d86e60f14cc9d1a546805aa35797d66e7066
François Rousselet <francois.rousselet@iguanesolutions.com>
2022-05-23 21:19:53 +0200

    Add IPv6 support (#247)

--- opennebula/resource_opennebula_virtual_machine.go
+++ opennebula/resource_opennebula_virtual_machine.go
@@ -843,8 +843,8 @@ func flattenNICComputed(nic shared.NIC) map[string]interface{} {
 	ip, _ := nic.Get(shared.IP)
 	mac, _ := nic.Get(shared.MAC)
 	physicalDevice, _ := nic.GetStr("PHYDEV")
-	ip6_global, _ := nic.GetStr("IP6_GLOBAL")
-	ip6_link, _ := nic.GetStr("IP6_LINK")
+	ip6Global, _ := nic.GetStr("IP6_GLOBAL")
+	ip6Link, _ := nic.GetStr("IP6_LINK")
 	network, _ := nic.Get(shared.Network)
 
 	model, _ := nic.Get(shared.Model)
@@ -861,8 +861,8 @@ func flattenNICComputed(nic shared.NIC) map[string]interface{} {
 		"nic_id":                   nicID,
 		"network":                  network,
 		"computed_ip":              ip,
-		"computed_ip6_global":      ip6_global,
-		"computed_ip6_link":        ip6_link,
+		"computed_ip6_global":      ip6Global,
+		"computed_ip6_link":        ip6Link,
 		"computed_mac":             mac,
 		"computed_physical_device": physicalDevice,
 		"computed_model":           model,
@@ -1862,6 +1862,8 @@ func updateNIC(ctx context.Context, d *schema.ResourceData, meta interface{}) er
 		},
 		"network_id",
 		"ip",
+		"ip6_global",
+		"ip6_link",
 		"mac",
 		"security_groups",
 		"model",
